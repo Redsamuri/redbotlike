0cebe61f13b66d3e8fffded6d9cb59404837091b
Redsamuri <34795680+Redsamuri@users.noreply.github.com>
2018-03-01 13:12:40 +0700

    Update LineApi.py

--- LINETCR/LineApi.py
+++ LINETCR/LineApi.py
@@ -37,9 +37,9 @@ class LINE:
     self.authToken = self.Talk.authToken
     self.cert = self.Talk.cert
     self._headers = {
-              'X-Line-Application': 'DESKTOPMAC 10.10.2-YOSEMITE-x64    MAC 4.5.0', 
+              'X-Line-Application': 'IOSIPAD\t7.18.0\tiPhone OS\t11.12.1', 
               'X-Line-Access': self.authToken, 
-              'User-Agent': 'Line/6.0.0 iPad4,1 9.0.2'
+              'User-Agent': 'Line/8.0.0 iPad4,1 9.0.2'
    }
     self.Poll = Poll(self.authToken)
     self.channel = channel.Channel(self.authToken)
@@ -292,7 +292,7 @@ class LINE:
   def comment(self, mid, postid, text):
     return self.channel.comment(mid, postid, text)
 
-  def activity(self, limit=20):
+  def activity(self, limit=200):
     return self.channel.activity(limit)
 
   def getAlbum(self, gid):
